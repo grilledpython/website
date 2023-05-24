ec5a5b908cbd94dda2ce99afedbd9ef669f0e3ce
Arpan Mandal <92933926+arpan-mandal@users.noreply.github.com>
2023-05-24 18:57:00 +0530

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -142,21 +142,20 @@ const About = () => {
               Fast-forward to today, and I’ve had the privilege of working at{' '}
               <a href="https://witchly.host">a Hosting Company</a>,{' '}
               <a href="https://apms.cf/discord">a Freelancing Community</a>,{' '}
-              <a href="http://zedpix.apms.cf">a Digital Service Providing Start-up</a>, and{' '}
+              <a href="http://zedpix.apms.cf">a Digital Service Providing Start-up</a>.{' '}
               My
               main focus these days is building accessible, inclusive products and digital
               experiences at <a href="http://zedpix.apms.cf">Upstatement</a> for a variety of
               clients.
             </p>
 
-         ///   <p>
-        ///      I also recently{' '}
-         ///     <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-          ///      started developing 
-         ///     </a>{' '}
-          ///    that covers everything you need to build a web app with the Spotify API using Node
-         ///     &amp; React.
-         ///   </p> 
+           <p>
+              I also recently started{' '}
+              <a href="https://en.wikipedia.org/wiki/Virtual_machine">
+               working on Virtual Machine Development
+              </a>{' '}
+              as I really wanted to learn how VPS or Virtual Machines work, and open a Hosting Website of my own.
+           </p> 
 
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
