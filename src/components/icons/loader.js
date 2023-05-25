48b1286378863fc7d87d1eaf323b32dce336b722
Arpan Mandal <92933926+arpan-mandal@users.noreply.github.com>
2023-05-25 11:42:14 +0530

    Update loader.js

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -4,7 +4,7 @@ const IconLoader = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <title>Loader Logo</title>
     <g>
-      <g id="B" transform="translate(15.000000, 15.000000)">
+      <g id="B" transform="translate(30.000000, 15.000000)">
         <path d="M7.47425 44H0.485618L16.1887 0.363635H23.7953L39.4984 44H32.5098L20.1731 8.28977H19.8322L7.47425 44ZM8.64613 26.9119H31.3166V32.4517H8.64613V26.9119Z" fill="#64FFDA"/>
       </g>
       <path
