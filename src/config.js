900bdae0e275a7c06237346990061c10c755a4d8
Arpan Mandal <92933926+arpan-mandal@users.noreply.github.com>
2023-05-25 08:43:22 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  email: 'me@arpanm.tech',
+  email: 'brittany.chiang@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/arpan-mandal',
+      url: 'https://github.com/bchiang7',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/grilledpython',
+      url: 'https://www.instagram.com/bchiang7',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/ApCozmic',
+      url: 'https://twitter.com/bchiang7',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/arpan-mandal-b40b07270',
+      url: 'https://www.linkedin.com/in/bchiang7',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/galaxy-gazer
+      url: 'https://codepen.io/bchiang7',
     },
   ],
 
