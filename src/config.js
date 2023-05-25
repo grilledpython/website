1f14d3236b86dd6d025ee9cfc992d490e2dc7334
Arpan Mandal <92933926+arpan-mandal@users.noreply.github.com>
2023-05-25 08:48:21 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,23 +4,23 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/arpan-mandal',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/grilledpython',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/ApCozmic',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/arpan-mandal-b40b07270',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://codepen.io/galaxy-gazer',
     },
   ],
 
