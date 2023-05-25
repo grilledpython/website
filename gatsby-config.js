19ab9ef7eca3a8d6d701cab1b2802a669a5ba1d0
Arpan Mandal <92933926+arpan-mandal@users.noreply.github.com>
2023-05-25 11:59:40 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Arpan Mandal',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Arpan Mandal is a Developer who specializes in Website Development and occasionally makes projects in Python or JavaScript.',
+    siteUrl: 'https://arpan.codes', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@ApCozmic',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Arpan Mandal',
+        short_name: 'Arpan Mandal',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
